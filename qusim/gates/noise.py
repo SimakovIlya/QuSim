66cb94957a4dcd3d604a6df1c2fa73c74065a7f5
SimakovIlya <50998922+SimakovIlya@users.noreply.github.com>
2020-07-08 13:10:02 +0300

    upd Ry noise PTM

--- qusim/gates/noise.py
+++ qusim/gates/noise.py
@@ -28,3 +28,19 @@ def R_dephasing_11_mse(phi): # transfer dephasing matrix for 2 qubits in Pauli b
     A.data[13, 13] = a
     A.data[14, 14] = a
     return(A)
+
+
+def add_noise_Ry(Ry, dtheta):
+    '''
+    PTM Ry(theta) (Qobj):
+    1,      0,      0,     0
+    0, cos(theta),  0, sin(theta)
+    0,      0,      1,     0
+    0, -sin(theta), 0, cos(theta)
+    '''
+    a = np.exp(-dtheta**2/2)
+    Ry.data[1, 1] *= a
+    Ry.data[1, 3] *= a
+    Ry.data[3, 1] *= a
+    Ry.data[3, 3] *= a
+    return(Ry)
