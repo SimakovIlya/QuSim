8c560b6acbf9ccb8c33f4759bcc7407dcd5bfc67
Ilya Simakov <50998922+SimakovIlya@users.noreply.github.com>
2021-02-02 20:49:09 +0300

    add Rx gate

--- qusim/gates/gates_1q.py
+++ qusim/gates/gates_1q.py
@@ -19,6 +19,15 @@ def Ry_gate(theta, theta_error = 0):
 
 
 
+def Rx_gate(theta, theta_error = 0):
+    '''
+    Returns Pauli trasfer matrix of Rx(theta) gate
+    '''
+    return to_Pauli_T_matrix(np.array([[np.cos(theta/2)*np.exp(-theta_error**2/8), -1j*np.sin(theta/2)*np.exp(-theta_error**2/8)],
+                                       [-1j*np.sin(theta/2)*np.exp(-theta_error**2/8), np.cos(theta/2)*np.exp(-theta_error**2/8)]]))
+
+
+
 
 def Hadamard_gate():
     '''
