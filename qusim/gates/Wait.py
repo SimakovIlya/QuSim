496254f5c7e52b73b3263bad8ebd9296b4ad2cbd
Ilya Simakov <50998922+SimakovIlya@users.noreply.github.com>
2022-12-17 23:51:58 +0300

    'upd_T1_Tf_noise'

--- qusim/gates/Wait.py
+++ qusim/gates/Wait.py
@@ -12,7 +12,7 @@ def Wait(t, T1, Tf):
     Returns:
         Qobj: trasfer matrix of idling (4 x 4)
     '''
-    R = Qobj(RT1(t, T1)@RTf(t, Tf))
+    R = RT1(t, T1)*RTf(t, Tf)
     return(R)
 
 
@@ -21,15 +21,15 @@ def Wait(t, T1, Tf):
 def RT1(t, T1): # transfer matrix amplitude damping in Pauli basis
     #print(t, T1)
     p = 1 - np.exp(-t/T1)
-    return(np.array([[1, 0, 0, 0],
+    return(Qobj(np.array([[1, 0, 0, 0],
                      [0, np.sqrt(1 - p), 0, 0],
                      [0, 0, np.sqrt(1 - p), 0],
-                     [p, 0, 0, 1 - p]]))
+                     [p, 0, 0, 1 - p]])))
 
 
 def RTf(t, Tf): # transfer matrix phase damping in Pauli basis
     p = 1 - np.exp(-2*t/Tf)
-    return(np.array([[1, 0, 0, 0],
+    return(Qobj(np.array([[1, 0, 0, 0],
                      [0, np.sqrt(1 - p), 0, 0],
                      [0, 0, np.sqrt(1 - p), 0],
-                     [0, 0, 0, 1]]))
+                     [0, 0, 0, 1]])))
