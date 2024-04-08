7ffd3da9715f10356178937d6722e5293e0b78c0
Ilya Simakov <50998922+SimakovIlya@users.noreply.github.com>
2024-04-08 14:34:29 +0300

    add_3Q_4Q_bugfix

--- qusim/Pauli_basis/to_Pauli_T_matrix.py
+++ qusim/Pauli_basis/to_Pauli_T_matrix.py
@@ -16,7 +16,7 @@ def to_Pauli_T_matrix(O):
         for i in range(0, 4):
             for j in range(0, 4):
                 T[i, j] = 1 / 2 * np.trace(sigma[i] @ O @ sigma[j] @ np.conj(O.T))
-    if (O.shape[0] == 4):
+    elif (O.shape[0] == 4):
         T = np.identity(16, dtype=complex)
         sigma2d = np.empty((16, 4, 4), complex)
         for i in range(0, 4):
@@ -25,33 +25,26 @@ def to_Pauli_T_matrix(O):
         for i in range(0, 16):
             for j in range(0, 16):
                 T[i, j] = 1 / 4 * np.trace(sigma2d[i, :, :] @ O @ sigma2d[j, :, :] @ np.transpose(O.conj()))
-    if (O.shape[0] == 8):
+    elif (O.shape[0] == 8):
         T = np.identity(64, dtype=complex)
+        sigma3d = np.empty((4 ** 3, 8, 8), complex)
+        for i in range(4):
+            for j in range(4):
+                for k in range(4):
+                    sigma3d[16 * i + 4 * j + k] = np.kron(np.kron(sigma[i], sigma[j]), sigma[k])
         for i in range(0, 64):
             for j in range(0, 64):
                 T[i, j] = 1 / 8 * np.trace(sigma3d[i, :, :] @ O @ sigma3d[j, :, :] @ np.transpose(O.conj()))
-    if (O.shape[0] == 16):
+    elif (O.shape[0] == 16):
         T = np.identity(256, dtype=complex)
+        sigma4d = np.empty((4 ** 4, 16, 16), complex)
+        for i in range(4):
+            for j in range(4):
+                for k in range(4):
+                    for m in range(4):
+                        sigma4d[64 * i + 16 * j + 4 * k + m] = np.kron(np.kron(np.kron(sigma[i], sigma[j]), sigma[k]),
+                                                                       sigma[m])
         for i in range(0, 256):
             for j in range(0, 256):
                 T[i, j] = 1 / 16 * np.trace(sigma4d[i, :, :] @ O @ sigma4d[j, :, :] @ np.transpose(O.conj()))
     return np.real(T)
-
-
-sigma2d = np.empty((16, 4, 4), complex)
-for i in range(0, 4):
-    for j in range(0, 4):
-        sigma2d[4 * i + j, :, :] = np.kron(sigma[i], sigma[j])
-
-sigma3d = np.empty((4 ** 3, 8, 8), complex)
-for i in range(4):
-    for j in range(4):
-        for k in range(4):
-            sigma3d[16 * i + 4 * j + k] = np.kron(np.kron(sigma[i], sigma[j]), sigma[k])
-
-sigma4d = np.empty((4 ** 4, 16, 16), complex)
-for i in range(4):
-    for j in range(4):
-        for k in range(4):
-            for m in range(4):
-                sigma4d[64 * i + 16 * j + 4 * k + m] = np.kron(np.kron(np.kron(sigma[i], sigma[j]), sigma[k]), sigma[m])
